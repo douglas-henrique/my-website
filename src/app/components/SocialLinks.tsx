867a06e1e72df1e21f277f237cd495898d5f104e
Douglas Henrique <douglas.henriquect@outlook.com>
2023-05-29 22:33:21 -0300

    applying select style

--- src/app/components/SocialLinks.tsx
+++ src/app/components/SocialLinks.tsx
@@ -8,25 +8,25 @@ const SocialLinks = () => {
     <div className='max-[600px]:w-11/12 max-[600px]:flex-col max-[600px]:gap-2 max-[600px]:mt-10 h-auto md:w-auto flex md:flex-row gap-10 mt-2'>
       <Link href={`https://twitter.com/dougyoshii`}>
         <div className='flex flex-row gap-2 items-center p-1 rounded-md'>
-          <FaTwitterSquare className='hover:cursor-pointer' size={32} />
+          <FaTwitterSquare className='hover:cursor-pointer' size={24} />
           <label className='hover:cursor-pointer' >Twitter</label>
         </div>
       </Link>
       <Link href={`https://github.com/douglas-henrique`}>
         <div className='flex flex-row gap-2 items-center p-1 rounded-md'>
-          <FaGithubSquare className='hover:cursor-pointer' size={32} />
+          <FaGithubSquare className='hover:cursor-pointer' size={24} />
           <label className='hover:cursor-pointer' >GitHub</label>
         </div>
       </Link>
       <Link href={`https://www.youtube.com/channel/UCIMpIRnb3xlmuZ2_rkdUttA`}>
         <div className='flex flex-row gap-2 items-center p-1 rounded-md '>
-          <FaYoutubeSquare className='hover:cursor-pointer' size={32} />
+          <FaYoutubeSquare className='hover:cursor-pointer' size={24} />
           <label className='hover:cursor-pointer'>Youtube</label>
         </div>
       </Link>
       <Link href={`https://www.linkedin.com/in/douglas-henrique-532258230/`}>
         <div className='flex flex-row gap-2 items-center p-1 rounded-md'>
-          <FaLinkedin size={32} className='hover:cursor-pointer' />
+          <FaLinkedin size={24} className='hover:cursor-pointer' />
           <label className='hover:cursor-pointer '>Linkedin</label>
         </div>
       </Link>

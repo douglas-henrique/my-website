570cb2387607a8d87786d56eaf2d07015c14a9ab
Douglas Henrique <douglas.henriquect@outlook.com>
2023-05-29 21:30:49 -0300

    applying favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
